fac22a1ffcc8726e0e774339f50de441c4c8ee11
unknown <leo091945@gmail.com>
2022-03-20 22:14:10 -0300

    Starting with load and save

--- src/com/omg/main/Menu.java
+++ src/com/omg/main/Menu.java
@@ -7,6 +7,13 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Menu {
 	
@@ -18,6 +25,75 @@ public class Menu {
 	
 	public Menu() {
 		icon = Game.spritesheet.getSprite(32, 0, 16, 16);
+	}
+	
+	public static void applySave(String str) {
+		String[] apl = str.split("/");
+		for(int i = 0; i < apl.length; i++) {
+			String[] apl1 = apl[i].split(":");
+			switch(apl1[0]) 
+			{
+				case "level":
+					World.worldRestart("map"+apl[2]+".png");
+					Game.gameState = "normal";
+			
+			}
+		}
+	}
+	
+	public static String loadGame(int encode) {
+		String line = null;
+		File file = new File("save.txt");
+		if(file.exists()) {
+			try {
+				String singleLine = null;
+				BufferedReader br = new BufferedReader(new FileReader("save.txt"));
+				try {	
+					while((singleLine = br.readLine()) != null) {
+						String [] tr = singleLine.split(":");
+						char[] val = tr[1].toCharArray();
+						tr[1] = "";
+						for(int i = 0; i < val.length; i++) {
+							val[i] -= encode;
+							tr[1] += val[i];
+						}
+						line += tr[0];
+						line +=":";
+						line += tr[1];
+						line += "/";
+					}
+				}catch(IOException e) {}
+				
+			}catch(FileNotFoundException e) {}
+		}
+		return line;
+		
+	}
+	
+	public static void saveGame(String[] val1, int[] val2,int encode) {
+		BufferedWriter write = null;
+		try {
+			write = new BufferedWriter(new FileWriter("save.txt"));
+		}catch(IOException e) {}
+		for(int i = 0; i < val1.length; i++) {
+			String current = val1[i];
+			current += ":";
+			char[] value = Integer.toString(val2[i]).toCharArray();
+			for(int j = 0 ; j < value.length ; j++) {
+				value[j] += encode;
+				current += value[j];
+			}
+			try {
+				write.write(current);
+				if(i < val1.length - 1) {
+					write.newLine();
+				}
+			}catch(IOException e) {}
+		}
+		try {
+			write.flush();
+			write.close();
+		}catch(IOException e) {}
 	}
 	
 	public void tick() {
